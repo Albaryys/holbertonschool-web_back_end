3ff5a992f0bd210e1daa97d2001441930c5a19f6
Albaryys <6205@holbertonstudents.com>
2023-08-28 10:59:36 +0200

    removed blank line

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -2,7 +2,6 @@
 """takes a float n as argument and returns the floor of the float"""
 
 
-
 def floor(n: float) -> int:
     """Return largest int value less than or equal to n."""
     return int(n) if n >= 0 else int(n) - 1
