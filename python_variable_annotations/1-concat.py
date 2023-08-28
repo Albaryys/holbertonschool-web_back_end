f8db3fada282190c6709cd3ece74bd11bf15b5ec
Albaryys <6205@holbertonstudents.com>
2023-08-28 10:39:41 +0200

    add task 1 concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""takes a string str1 and a string str2 as arguments"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Return concatenation of str1 and str2."""
+    return str1 + str2
