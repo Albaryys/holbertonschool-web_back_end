f573ef84ade2e4bf452d44954360fe230db03de4
Albarys_ <a.tily.pro@gmail.com>
2023-09-14 11:57:27 +0200

    add task 0 and his main with gitignore

---
+++ ES6_classes/0-classroom.js
@@ -0,0 +1,9 @@
+// 0-classroom.js
+
+// Implémentation de la classe ClassRoom
+export default class ClassRoom {
+    constructor(maxStudentsSize) {
+      this._maxStudentsSize = maxStudentsSize;
+    }
+  }
+  
